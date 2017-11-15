4dce8b74ff9fc224f1f863627c983e480c40af22
tpheaktra.pbh <tpheaktra.pbh@gmail.com>
2017-11-15 09:05:19 +0700

    upload new

---
+++ resources/views/front-end/tours.blade.php
@@ -0,0 +1,109 @@
+@section('title', 'tours')
+@extends('layouts.front-end')
+
+@section('content')
+<div class="wrapper">
+    <div class="content home">
+
+        <div class="section hero-banner">
+            <div class="iAmTest">
+               
+                <div class="parallax">
+                    <img src="http://placehold.it/1920x700">
+                </div>
+                <div class="parallax">
+                    <img src="http://placehold.it/1920x700">
+                </div>
+                <div class="parallax">
+                    <img src="http://placehold.it/1920x700">
+                </div>
+               
+            </div>
+        </div>
+
+      
+
+        <div class="sub-business">
+           
+                <div class="sub-list who-we-are">
+                    <div class="container">
+                        <div class="row">
+                            <div class="detail col-sm-5 col-xs-12 fadeInDown">
+                                <div class="article">
+                                    <img  src="http://placehold.it/580x480">
+                                </div>
+                            </div>
+                            <div class="desc col-sm-7 col-xs-12 fadeInDown">
+                                <div class="article">
+                                    <h3>EIFFEL TOWER</h3>
+                                    <p>Eiffel Tower & Western Paris Guy de Maupassant, a French author, hated the Eiffel Tower so much he used to eat lunch at the bottom of it almost every day because it was the only place in Paris where he couldn’t see the tower. Today, the Eiffel Tower is a national icon and the title of ‘monstrosity’ now belongs to the Montparnasse Tower which summit offers a spectacular view over Paris. </p>
+                                </div>
+                            </div>
+
+                        </div>
+                    </div>
+                </div>
+
+                <div class="sub-list who-we-are">
+                    <div class="container">
+                        <div class="row">
+                            <div class="detail col-sm-5 col-xs-12 fadeInDown">
+                                <div class="article">
+                                    <img  src="http://placehold.it/580x480">
+                                </div>
+                            </div>
+                            <div class="desc col-sm-7 col-xs-12 fadeInDown">
+                                <div class="article">
+                                    <h3>EIFFEL TOWER</h3>
+                                    <p>Eiffel Tower & Western Paris Guy de Maupassant, a French author, hated the Eiffel Tower so much he used to eat lunch at the bottom of it almost every day because it was the only place in Paris where he couldn’t see the tower. Today, the Eiffel Tower is a national icon and the title of ‘monstrosity’ now belongs to the Montparnasse Tower which summit offers a spectacular view over Paris. </p>
+                                </div>
+                            </div>
+
+                        </div>
+                    </div>
+                </div>
+
+                <div class="sub-list who-we-are">
+                    <div class="container">
+                        <div class="row">
+                            <div class="detail col-sm-5 col-xs-12 fadeInDown">
+                                <div class="article">
+                                    <img  src="http://placehold.it/580x480">
+                                </div>
+                            </div>
+                            <div class="desc col-sm-7 col-xs-12 fadeInDown">
+                                <div class="article">
+                                    <h3>EIFFEL TOWER</h3>
+                                    <p>Guy de Maupassant, a French author, hated the Eiffel Tower so much he used to eat lunch at the bottom of it almost every day because it was the only place in Paris where he couldn’t see the tower. Today, the Eiffel Tower is a national icon and the title of ‘monstrosity’ now belongs to the Montparnasse Tower which summit offers a spectacular view over Paris. </p>
+                                </div>
+                            </div>
+
+                        </div>
+                    </div>
+                </div>
+
+                <div class="sub-list who-we-are">
+                    <div class="container">
+                        <div class="row">
+                            <div class="detail col-sm-5 col-xs-12 fadeInDown">
+                                <div class="article">
+                                    <img  src="http://placehold.it/580x480">
+                                </div>
+                            </div>
+                            <div class="desc col-sm-7 col-xs-12 fadeInDown">
+                                <div class="article">
+                                    <h3>EIFFEL TOWER</h3>
+                                    <p>Eiffel Tower & Western Paris Guy de Maupassant, a French author, hated the Eiffel Tower so much he used to eat lunch at the bottom of it almost every day because it was the only place in Paris where he couldn’t see the tower. Today, the Eiffel Tower is a national icon and the title of ‘monstrosity’ now belongs to the Montparnasse Tower which summit offers a spectacular view over Paris. </p>
+                                </div>
+                            </div>
+
+                        </div>
+                    </div>
+                </div>
+          
+        </div>
+
+
+    </div>
+</div>
+@stop
